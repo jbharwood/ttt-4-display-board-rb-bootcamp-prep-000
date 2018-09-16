f6338d7fc4a3c32bac83359a67466754f4a4cd77
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-16 03:12:49 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board_spec
+def display_board_spec(board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " " ]
   puts board
 end
